85d0b576ea1ddec8061639ee266aa325e245141c
OleksiiTymchenko <tta19764@gmail.com>
2025-04-22 13:36:04 +0300

    Slightly adjusted the ui elements, add ActionButtonControl user control

---
+++ HumanRecourcesApp/HumanRecourcesApp/Controls/ActionButtonControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace HumanResourcesApp.Controls
+{
+    public partial class ActionButtonControl : UserControl
+    {
+        public ActionButtonControl()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ButtonCommandProperty =
+            DependencyProperty.Register("ButtonCommand", typeof(ICommand), typeof(ActionButtonControl), new PropertyMetadata(null));
+
+        public ICommand ButtonCommand
+        {
+            get => (ICommand)GetValue(ButtonCommandProperty);
+            set => SetValue(ButtonCommandProperty, value);
+        }
+
+        public static readonly DependencyProperty ButtonContentProperty =
+            DependencyProperty.Register("ButtonContent", typeof(object), typeof(ActionButtonControl), new PropertyMetadata("Button"));
+
+        public object ButtonContent
+        {
+            get => GetValue(ButtonContentProperty);
+            set => SetValue(ButtonContentProperty, value);
+        }
+
+        public static readonly DependencyProperty ButtonPaddingProperty =
+    DependencyProperty.Register(
+        nameof(ButtonPadding),
+        typeof(Thickness),
+        typeof(ActionButtonControl),
+        new PropertyMetadata(new Thickness(12, 8, 12, 8)));
+
+        public Thickness ButtonPadding
+        {
+            get => (Thickness)GetValue(ButtonPaddingProperty);
+            set => SetValue(ButtonPaddingProperty, value);
+        }
+
+
+    }
+}
